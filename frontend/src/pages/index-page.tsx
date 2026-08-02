e9864b03e6b83885621f92abb25392b88af8283a
orange_ch <97601771+orange-ch-cya@users.noreply.github.com>
2026-08-03 00:10:10 +0800

    Update index-page.tsx

--- frontend/src/pages/index-page.tsx
+++ frontend/src/pages/index-page.tsx
@@ -787,7 +787,7 @@ export function IndexPage() {
 																置顶
 															</span>
 														) : null}
-														<a className="truncate text-lg font-semibold hover:underline" href={`/p?id=${p.id}`}>
+														<a className="truncate text-lg font-semibold hover:underline" href={`/p/${p.id}`}>
 															{p.title}
 														</a>
 													</div>
